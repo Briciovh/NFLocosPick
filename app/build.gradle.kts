7239a11a1979f86c7d5b39f27b21c0f8b2ced920
Bricio Velázquez <briciovh@hotmail.com>
2026-07-14 17:22:38 -0600

    Dependency Updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,16 +9,12 @@ plugins {
 
 android {
     namespace = "com.softeen.nflocospicks"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.softeen.nflocospicks"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
